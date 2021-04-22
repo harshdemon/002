2a06e49ffb712cbd29bd5d5881578ab8e51fc3c6
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-22 23:44:11 +0530

    Update shayari.py

--- SaitamaRobot/modules/shayari.py
+++ SaitamaRobot/modules/shayari.py
@@ -4,27 +4,24 @@ from telegram import Message, Chat, Update, Bot, MessageEntity
 from SaitamaRobot import dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 
-SFW_STRINGS = (
-   "Saans bhi loon to uski mehak aati hai…. usne thukraya hai mujhe itne qareeb aane k baad..",
-   "Hum toh bane hii tabah hone k liye the,, tumhara milna toh bas ek bahana tha…!!",
-   "Jo Ho Ijazat To Tum Se ek Baat Punchhu…… Jo Hum Se Ishq Seekha Tha Woh Ab Tum Kis Se Karte Ho….!!!!",
-   "Bandh mutthi me Ret ki Tarah, Bhoola diya tumne zara zara karke..!!",
-   "mere halaat par muskurate ho, Baddua hAI tujHe ishq ho jaAye.",
-   "Teri Mohabbat Bhi Kiraye Ke Ghar Ki Tarah Thi, Kitna Bhi Sajaya, Par Mere Na Hue.",
-   "Tu agar khawab tha mera to bata kyu meri neend se bahaar nikla",
-   "fāsla nazroñ kā dhoka bhī to ho saktā hai vo mile yā na mile haath baḌhā kar dekho",
-  )
+SHAYARI_STRINGS = (
+  "Har Tanha Raat Mein Ek Naam Yaad Aata Hai Kabhi Subhah To Kabhi Shaam Yaad Aata Hai,Jab Sochte Hain Kar Lein Dobara Mohabbat,Fir Pehli Mohabbat Ka Anzaam Yaad Aata Hai.",
+  "Humein Shayar Samajh Ke Yoon Najar Andaz Mat Kariye,Najar Hum Fer Le To Husn Ka Baazar Gir Jayega.",
+  "Humari Haisiyat Ka Andaza Tum Ye Jaan Ke Laga Lo,Hum Kabhi Unke Nahi Hote Jo Har Kisi Ke Ho Gaye.",
+  "Iss Se Zyada Tujhe Aur Kitna Qareeb Laaun Main,Ki Tujhe Dil Mein Rakh Kar Bhi Mera Dil Nahi Bharta.",
+  "Ye Zindagi Hasin Hai Iss Se Pyar Karo,Abhi Hai Raat To Subah Ka Intezar Karo,Wo Pal Bhi Ayega Jiski Khwahish Hai Aapko,Rab Par Rakho Bharosa Waqt Par Aitbar Karo.",
+)
 
 @run_async
 def shayari(bot: Bot, update: Update):
     bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
     message = update.effective_message
     if message.reply_to_message:
-      message.reply_to_message.reply_text(random.choice(SFW_STRINGS))
+      message.reply_to_message.reply_text(random.choice(SHAYARI_STRINGS))
     else:
-      message.reply_text(random.choice(SFW_STRINGS))
+      message.reply_text(random.choice(SHAYARI_STRINGS))
 
 
-SHAYARI_HANDLER = DisableAbleCommandHandler("shayari", dhokha)
+SHAYARI_HANDLER = DisableAbleCommandHandler("shayari", shayari)
 
 dispatcher.add_handler(SHAYARI_HANDLER)
