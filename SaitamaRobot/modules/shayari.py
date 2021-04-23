419d060444c5e0ba88ec8aa7bbbafcefeed1da07
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-23 14:10:50 +0530

    Update shayari.py

--- SaitamaRobot/modules/shayari.py
+++ SaitamaRobot/modules/shayari.py
@@ -9,20 +9,12 @@ from telegram.ext import CallbackContext, run_async
 from tswift import Song
 from telegram.error import BadRequest
 
-import SaitamaRobot.modules.shayari_strings as fun_strings
+import SaitamaRobot.modules.shayari_strings as shayari_strings
 from SaitamaRobot import dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
 from SaitamaRobot.modules.helper_funcs.chat_status import (is_user_admin)
 from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-
-SHAYARI_STRINGS = (
-  "Har Tanha Raat Mein Ek Naam Yaad Aata Hai Kabhi Subhah To Kabhi Shaam Yaad Aata Hai,Jab Sochte Hain Kar Lein Dobara Mohabbat,Fir Pehli Mohabbat Ka Anzaam Yaad Aata Hai.",
-  "Humein Shayar Samajh Ke Yoon Najar Andaz Mat Kariye,Najar Hum Fer Le To Husn Ka Baazar Gir Jayega.",
-  "Humari Haisiyat Ka Andaza Tum Ye Jaan Ke Laga Lo,Hum Kabhi Unke Nahi Hote Jo Har Kisi Ke Ho Gaye.",
-  "Iss Se Zyada Tujhe Aur Kitna Qareeb Laaun Main,Ki Tujhe Dil Mein Rakh Kar Bhi Mera Dil Nahi Bharta.",
-  "Ye Zindagi Hasin Hai Iss Se Pyar Karo,Abhi Hai Raat To Subah Ka Intezar Karo,Wo Pal Bhi Ayega Jiski Khwahish Hai Aapko,Rab Par Rakho Bharosa Waqt Par Aitbar Karo.",
-)
 
 @run_async
 def shayari(bot: Bot, update: Update):
