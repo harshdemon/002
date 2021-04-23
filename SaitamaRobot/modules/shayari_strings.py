30b92003597ae24a10a3e5b1a9cfc35d6adc4256
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-23 18:14:35 +0530

    Update shayari_strings.py

--- SaitamaRobot/modules/shayari_strings.py
+++ SaitamaRobot/modules/shayari_strings.py
@@ -4,4 +4,8 @@ SHAYARI = (
   "Humari Haisiyat Ka Andaza Tum Ye Jaan Ke Laga Lo,Hum Kabhi Unke Nahi Hote Jo Har Kisi Ke Ho Gaye.",
   "Iss Se Zyada Tujhe Aur Kitna Qareeb Laaun Main,Ki Tujhe Dil Mein Rakh Kar Bhi Mera Dil Nahi Bharta.",
   "Ye Zindagi Hasin Hai Iss Se Pyar Karo,Abhi Hai Raat To Subah Ka Intezar Karo,Wo Pal Bhi Ayega Jiski Khwahish Hai Aapko,Rab Par Rakho Bharosa Waqt Par Aitbar Karo.",
+  "Hausle Bhi Kisi Hakeem Se Kam Nahi Hote,Har Takleef Mein Takat Ki Davaa Dete Hain.",
+  "Ek Sooraj Tha Ki Taaro Ke Gharane Se Uthha,Aankh Hairan Hai Kya Shakhs Zamane Se Uthha.",
+  "Kaam Karo Aisa Ki Ek Pehchaan Ban Jaye,har Kadam Aisa Chalo Ki Nishan Ban Jaye,Yehan Zindagi To Har Koi Kaat Leta Hai,Zindagi Jiyo Iss Kadar Ki Misaal Ban Jaye.",
+  "Bade Hi Ajeeb Hain Ye Zindagi Ke Raaste,Anjane Mod Par Kuchh Log Apne Ban Jate Hain,Milne Ki Khushi Dein Ya Na Dein,Magar Bichadne Ka Gam Zaroor De Jate Hain.",
 )
