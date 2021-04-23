abbc3163a43705298f4a0caa8a853ef6406eee22
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-23 14:09:52 +0530

    Create shayari_strings.py

---
+++ SaitamaRobot/modules/shayari_strings.py
@@ -0,0 +1,7 @@
+SHAYARI_STRINGS = (
+  "Har Tanha Raat Mein Ek Naam Yaad Aata Hai Kabhi Subhah To Kabhi Shaam Yaad Aata Hai,Jab Sochte Hain Kar Lein Dobara Mohabbat,Fir Pehli Mohabbat Ka Anzaam Yaad Aata Hai.",
+  "Humein Shayar Samajh Ke Yoon Najar Andaz Mat Kariye,Najar Hum Fer Le To Husn Ka Baazar Gir Jayega.",
+  "Humari Haisiyat Ka Andaza Tum Ye Jaan Ke Laga Lo,Hum Kabhi Unke Nahi Hote Jo Har Kisi Ke Ho Gaye.",
+  "Iss Se Zyada Tujhe Aur Kitna Qareeb Laaun Main,Ki Tujhe Dil Mein Rakh Kar Bhi Mera Dil Nahi Bharta.",
+  "Ye Zindagi Hasin Hai Iss Se Pyar Karo,Abhi Hai Raat To Subah Ka Intezar Karo,Wo Pal Bhi Ayega Jiski Khwahish Hai Aapko,Rab Par Rakho Bharosa Waqt Par Aitbar Karo.",
+)
