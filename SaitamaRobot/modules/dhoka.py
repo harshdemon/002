a63d99b361014ba663d84d601114ab4dfb1f87f5
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-23 14:34:51 +0530

    Update dhoka.py

--- SaitamaRobot/modules/dhoka.py
+++ SaitamaRobot/modules/dhoka.py
@@ -1,30 +1,27 @@
+#khud banaya hai kang karna hai toh credit de dena
+import html
 import random
-from telegram.ext import run_async, Filters
-from telegram import Message, Chat, Update, Bot, MessageEntity
+import time
+
+from typing import Optional
+from telegram import ParseMode, Update, ChatPermissions
+from telegram.ext import CallbackContext, run_async
+from tswift import Song
+from telegram.error import BadRequest
+
+import SaitamaRobot.modules.dhoka_strings as dhoka_strings
 from SaitamaRobot import dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-
-SFW_STRINGS = (
-   "Saans bhi loon to uski mehak aati hai…. usne thukraya hai mujhe itne qareeb aane k baad..",
-   "Hum toh bane hii tabah hone k liye the,, tumhara milna toh bas ek bahana tha…!!",
-   "Jo Ho Ijazat To Tum Se ek Baat Punchhu…… Jo Hum Se Ishq Seekha Tha Woh Ab Tum Kis Se Karte Ho….!!!!",
-   "Bandh mutthi me Ret ki Tarah, Bhoola diya tumne zara zara karke..!!",
-   "mere halaat par muskurate ho, Baddua hAI tujHe ishq ho jaAye.",
-   "Teri Mohabbat Bhi Kiraye Ke Ghar Ki Tarah Thi, Kitna Bhi Sajaya, Par Mere Na Hue.",
-   "Tu agar khawab tha mera to bata kyu meri neend se bahaar nikla",
-   "fāsla nazroñ kā dhoka bhī to ho saktā hai vo mile yā na mile haath baḌhā kar dekho",
-  )
+from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from SaitamaRobot.modules.helper_funcs.chat_status import (is_user_admin)
+from SaitamaRobot.modules.helper_funcs.extraction import extract_user
 
 @run_async
-def dhokha(bot: Bot, update: Update):
-    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
-    message = update.effective_message
-    if message.reply_to_message:
-      message.reply_to_message.reply_text(random.choice(SFW_STRINGS))
-    else:
-      message.reply_text(random.choice(SFW_STRINGS))
+def dhoka(update: Update, context: CallbackContext):
+    update.message.reply_text(random.choice(dhoka_strings.DHOKA))
 
 
-DHOKHA_HANDLER = DisableAbleCommandHandler("dhokha", dhokha)
 
-dispatcher.add_handler(DHOKHA_HANDLER)
+DHOKA_HANDLER = DisableAbleCommandHandler("dhoka", dhoka)
+
+dispatcher.add_handler(DHOKA_HANDLER)
