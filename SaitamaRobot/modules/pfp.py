71aa541b34a01990af120087eef3f8f30da469c6
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-07-04 19:41:58 +0530

    Update pfp.py

--- SaitamaRobot/modules/pfp.py
+++ SaitamaRobot/modules/pfp.py
@@ -1,6 +1,6 @@
-from AsunaRobot.events import register
-from AsunaRobot import OWNER_ID
-from AsunaRobot import telethn as tbot
+from SaitamaRobot.events import register
+from SaitamaRobot import OWNER_ID
+from SaitamaRobot import telethn as tbot
 import os 
 from PIL import Image, ImageDraw, ImageFont
 
@@ -20,13 +20,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./AsunaRobot/resources/blackbg.jpg')
+    img = Image.open('./SaitamaRobot/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./AsunaRobot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./SaitamaRobot/resources/Chopsic.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -34,13 +34,13 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByYone.png"
+    fname2 = "LogoByZerotwo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @My_Asuna_Robot")
+    await tbot.send_file(event.chat_id, fname2, caption="Take your logo")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @AsunaRobotSupport, {e}')
+   await event.reply(f'Error., {e}')
 
 
 
@@ -60,13 +60,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./AsunaRobot/resources/blackbg.jpg')
+    img = Image.open('./SaitamaRobot/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./AsunaRobot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./SaitamaRobot/resources/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -74,13 +74,13 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByYone.png"
+    fname2 = "LogoByZerotwo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @My_Asuna_Robot")
+    await tbot.send_file(event.chat_id, fname2, caption="Take your logo")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @AsunaRobotSupport, {e}')
+   await event.reply(f'Error., {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
